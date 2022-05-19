9541c25bf87f1fd3064eefd6436e650cf445885e
Thomas Pisani <thomaspisani14@gmail.com>
2022-05-19 13:49:45 -0300

    Primera version

---
+++ src/main/java/com/example/jugadores_de_tenis_cris/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.jugadores_de_tenis_cris;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
