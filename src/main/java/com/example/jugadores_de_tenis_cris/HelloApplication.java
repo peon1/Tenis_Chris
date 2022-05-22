8adb0c71fc8d3929bfab949f4153d3735084cb9b
Thomas Pisani <thomaspisani14@gmail.com>
2022-05-22 17:52:20 -0300

    Dynamic players load

--- src/main/java/com/example/jugadores_de_tenis_cris/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.jugadores_de_tenis_cris;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
