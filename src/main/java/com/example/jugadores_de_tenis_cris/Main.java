f4c8bcfd154e030a733e5ee4a5f2f686e9ed316f
Thomas Pisani <thomaspisani14@gmail.com>
2022-05-29 12:50:53 -0300

    Hechos los cambios solicitados

--- src/main/java/com/example/jugadores_de_tenis_cris/Main.java
+++ src/main/java/com/example/jugadores_de_tenis_cris/Main.java
@@ -5,18 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+  static private int id;
+  static private int edad;
+   static private String nombre;
+   static private String apellido;
     public static void main(String[] args) {
-        boolean bandera = true;
+
         Scanner escaner = new Scanner(System.in);
         List <Persona> jugadores_Tenis = new ArrayList<Persona>();
-        while (bandera)
+
+        do
         {
 
 
-            int id;
-            int edad;
-            String nombre = "";
-            String apellido = "";
+
             System.out.println("Ingrese el ID del jugador");
             id = escaner.nextInt();
             System.out.println("Ingrese la edad del jugador");
@@ -29,16 +31,13 @@ public class Main {
             Persona jugador = new Persona(id,edad,nombre,apellido);
             jugadores_Tenis.add(jugador);
 
-            System.out.println("¿Deseas seguir agregando jugadores? 1 para si, otro numero para no");
-
-            if (escaner.nextInt() != 1)
-            {
-                bandera = false;
-            }
+            System.out.println("¿Deseas seguir agregando jugadores? S/N");
 
 
 
-        }
+
+
+        } while (escaner.next() == "S");
         System.out.println("Cantidad de jugadores registrados: " + jugadores_Tenis.size());
         for(int i = 0; i < jugadores_Tenis.size();i++)
         {
